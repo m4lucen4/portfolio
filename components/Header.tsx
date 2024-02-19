a2c4b8759c3858bc8acf292c8b210f3dea7e7f37
m4lucen4 <malucena@me.com>
2024-02-19 22:11:39 +0100

    feat: header and config styles

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import { navLinks } from '@/constants';
+import Link from 'next/link';
+import React from 'react';
+import NavLink from './NavLink';
+import { Phone } from 'lucide-react';
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between h-20 max-width border-b border-peach">
+      <Link href="/">
+        <h5 className="text-blue text-xl font-semibold">malucena.dev</h5>
+      </Link>
+      <nav className="flex items-center gap-12">
+        <ul className="flex item-center gap-8">
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <NavLink href={link.url} label={link.label} />
+            </li>
+          ))}
+        </ul>
+        <Link href={'/contact'} className="btn btn-primary">
+          <Phone size={24} />
+          Contacta
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
