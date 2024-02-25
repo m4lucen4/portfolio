bff18a51ba9edf23f2fd9eb631b791d7f26649bd
m4lucen4 <malucena@me.com>
2024-02-25 22:51:28 +0100

    feat: toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter, Manrope } from 'next/font/google';
 import './globals.css';
+import NextTopLoader from 'nextjs-toploader';
 
 const manrope = Manrope({ subsets: ['latin'] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`bg-peach-600 ${manrope.className}`}>{children}</body>
+      <body className={`bg-peach-600 ${manrope.className}`}>
+        <NextTopLoader color="#5700FF" showSpinner={false} />
+        {children}
+      </body>
     </html>
   );
 }
